262364cf52058efba759faa0e266e9b08a417d7e
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-08-12 11:48:18 +0430

    Example(article)

--- WS.py
+++ WS.py
@@ -14,7 +14,7 @@ L 1
 L2
 """
 from bs4 import BeautifulSoup
-# import re
+import re
 #
 # url = 'https://en.wikipedia.org/wiki/Mao_Zedong'
 # response = requests.get(url)
@@ -69,5 +69,36 @@ for table in ep_tables:
 
 for ep in episodes:
     print(ep)
+
+# ------------------------------------------------------
+"""
+L 4
+"""
+
+url = 'https://news.ycombinator.com/news'
+req = requests.get(url)
+content = BeautifulSoup(req.text, 'html.parser')
+
+articles = []
+
+for item in content.find_all('tr', class_='athing'):
+    item_a = item.find('a')
+    item_link = item_a.get('href') if item.a else None
+    item_text = item_a.get_text(strip=True) if item_a else None
+    next_row = item.find_next_sibling('tr')
+    item_scores = next_row.find('span', class_='score')
+    item_scores = item_scores.get_text(strip=True) if item_scores else '0 point'
+    item_comments = next_row.find('a', text=re.compile('\d+(&nbsp;|\s)comment(s?)'))
+    item_comments = item_comments.get_text(strip=True).replace('\xa0', ' ') if item_comments else '0 comment'
+
+    articles.append({
+        'link': item_link,
+        'title': item_text,
+        'score': item_scores,
+        'comments': item_comments,
+    })
+
+for article in articles:
+    print(article)
 
 # ------------------------------------------------------
