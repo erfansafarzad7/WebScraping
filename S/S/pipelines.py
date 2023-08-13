17c7bb277c2bbf6d7dffe7319b30e0c4c6d2633a
erfansafarzad7 <erfansafarzad7@gmail.com>
2023-08-13 20:16:44 +0430

    Scrapy-Pipelines(Example-countries)

--- S/S/pipelines.py
+++ S/S/pipelines.py
@@ -6,8 +6,31 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
+from scrapy.exceptions import DropItem
 
 
 class SPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect('countries.db')
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS countries(
+        name TEXT PRIMARY KEY, capital TEXT, population INTEGER
+        )""")
+
     def process_item(self, item, spider):
+        self.cur.execute("""
+        INSERT OR IGNORE INTO countries VALUES (?, ?, ?)
+        """, (item['name'], item['capital'], item['population']))
+        self.con.commit()
+        return item
+
+
+class PopulationPipeline:
+    def process_item(self, item, spider):
+        if int(item['population']) < 50_000_000:
+            raise DropItem('Population is less than 50M..')
         return item
